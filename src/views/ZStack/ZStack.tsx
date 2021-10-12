c45003d26c5fe69d2f9a945a88403c0d83993997
andrew-levy <alevy97@gmail.com>
2021-10-12 12:51:40 -0400

    bug fix: zstack container to be width of content

--- src/views/ZStack/ZStack.tsx
+++ src/views/ZStack/ZStack.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { LayoutChangeEvent, View } from 'react-native';
+import React from 'react';
+import { View } from 'react-native';
 import { ALIGNMENT_MAP } from '../../constants';
 import { ZStackProps } from '../../types';
 
@@ -8,15 +8,13 @@ export const ZStack = ({
   style,
   children,
 }: ZStackProps) => {
-  const [maxDimensions, setMaxDimensions] = useState({ width: 0, height: 0 });
   return (
     <View
       style={[
         {
           justifyContent: ALIGNMENT_MAP[alignment.vertical],
           alignItems: ALIGNMENT_MAP[alignment.horizontal],
-          width: maxDimensions.width,
-          height: maxDimensions.height,
+          alignSelf: 'center',
         },
         style,
       ]}
@@ -27,15 +25,6 @@ export const ZStack = ({
           style: {
             zIndex: i,
             position: i === 1 ? 'relative' : 'absolute',
-          },
-          onLayout: (event: LayoutChangeEvent) => {
-            const { width, height } = event.nativeEvent.layout;
-            if (width > maxDimensions.width) {
-              setMaxDimensions((prev) => ({ ...prev, width }));
-            }
-            if (height > maxDimensions.height) {
-              setMaxDimensions((prev) => ({ ...prev, height }));
-            }
           },
         })
       )}
