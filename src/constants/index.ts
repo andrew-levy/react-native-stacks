9b200bc5b4130adadc5637287d4d5faa8d0e102f
andrew-levy <alevy97@gmail.com>
2022-02-13 13:46:36 -0500

    added new alignment guides

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,17 @@
 export const ALIGNMENT_MAP = {
-  center: 'center',
-  leading: 'flex-start',
-  trailing: 'flex-end',
+  center: "center",
+  leading: "flex-start",
+  trailing: "flex-end",
+} as const;
+
+export const ZSTACK_ALIGNMENT_MAP = {
+  center: { justifyContent: "center", alignItems: "center" },
+  leading: { justifyContent: "center", alignItems: "flex-start" },
+  trailing: { justifyContent: "center", alignItems: "flex-end" },
+  top: { justifyContent: "flex-start", alignItems: "center" },
+  bottom: { justifyContent: "flex-start", alignItems: "center" },
+  topLeading: { justifyContent: "flex-start", alignItems: "flex-start" },
+  topTrailing: { justifyContent: "flex-start", alignItems: "flex-end" },
+  bottomLeading: { justifyContent: "flex-end", alignItems: "flex-start" },
+  bottomTrailing: { justifyContent: "flex-end", alignItems: "flex-end" },
 } as const;
--- src/views/ZStack/ZStack.tsx
+++ src/views/ZStack/ZStack.tsx
@@ -1,30 +1,24 @@
-import React from 'react';
-import { View } from 'react-native';
-import { ALIGNMENT_MAP } from '../../constants';
-import { ZStackProps } from '../../types';
+import React from "react";
+import { View } from "react-native";
+import { ZSTACK_ALIGNMENT_MAP } from "../../constants";
+import { ZStackProps } from "../../types";
 
 export const ZStack = ({
-  alignment = { vertical: 'center', horizontal: 'center' },
+  alignment = "center",
   style,
   children,
 }: ZStackProps) => {
   return (
-    <View
-      style={[
-        {
-          justifyContent: ALIGNMENT_MAP[alignment.vertical],
-          alignItems: ALIGNMENT_MAP[alignment.horizontal],
-          alignSelf: 'center',
-        },
-        style,
-      ]}
-    >
+    <View style={[{ ...ZSTACK_ALIGNMENT_MAP[alignment] }, style]}>
       {React.Children.map(children, (child, i) =>
         React.cloneElement(child, {
           ...child.props,
           style: {
             zIndex: i,
-            position: i === 1 ? 'relative' : 'absolute',
+            position:
+              i === 1 || React.Children.count(children) === 1
+                ? "relative"
+                : "absolute",
           },
         })
       )}
